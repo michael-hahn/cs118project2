bb8e719c81dcf9d76aa80546bc1eebb31d860a33
hubbub03 <anibuddy000@aol.com>
2015-05-31 19:34:18 -0700

    Now object oriented!

--- myrouter.cpp
+++ myrouter.cpp
@@ -1,175 +1,21 @@
-///* Sample UDP client */
-//
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdio.h>
 #include <iostream>
-#include <cstdio>
-#include <fstream>
-#include <cstring>
-#include <vector>
-#include <stdlib.h>
-#include <algorithm>
-#include <string>
-#include <sstream>
+#include "comm_link.h"
+#include "manager.h"
+
+
 using namespace std;
-
-struct neighboure
-{
-    string name;
-    int cost;
-    int port_from_me_to_neighboure;
-    int port_from_neighboure_to_me;
-};
-
-
 
 //for now: the user provides the "name of the router" {A,B,C,D,E,F}, we gotta figure out officially, how a router is supposed to know who s/he is
 int main(int argc, char**argv)
 {
-    int sockfd,n;
-    struct sockaddr_in servaddr,cliaddr;
-    char sendline[1000];
-    char recvline[1000];
-    
-    if (argc != 2)
-    {
-        cout << "You forgot to name the router" << endl;
-        exit(1);
-    }
-    
-    sockfd=socket(AF_INET,SOCK_DGRAM,0);
-    
-    bzero(&servaddr,sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
-    // servaddr.sin_port=htons(32000);
-    
-    //read through the file to inialize the node
-    ifstream inputfile;
-    char c;
-    string line;
-    inputfile.open("initialize.txt");
-    if(!inputfile)
-    {
-        cout << "An error occurered while attem[ting to open the file\n";
-        exit(1);
-    }
-    
-    int myport;
-    vector<neighboure> neighborhood;
-    
-    while(inputfile.get(c))
-    {
-        //parse here
-        while(true)
-        {
-            line = line + c;
-            if(!inputfile.get(c))
-            {
-                break;
-            }
-            if(c == '\n')
-            {
-                line = line + c;
-                break;
-            }
-        }
-        int length_of_line = (int)line.length();
-        char *input = new char[length_of_line+1];
-        
-        strcpy(input,line.c_str());
-        input[length_of_line+1] = '\0';
-        
-        char source_name[16];
-        char destination_name[16];
-        char s_source_port[16];
-        char s_cost[16];
-        int source_port, cost;
-        
-        //        cout << "here -> " << input ;
-        sscanf (input, "%8[^,],%8[^,],%8[^,],%8[^,]", source_name, destination_name, s_source_port, s_cost);
-        source_port = atoi(s_source_port);
-        cost = atoi(s_cost);
-        
-        //        cout << "source name - " << source_name << endl;
-        //        cout << "destination name - " << destination_name << endl;
-        //        cout << "source port - " << source_port << endl;
-        //        cout << "source cost - " << cost<< endl;
-        //        cout << endl;
-        line = "";
-        
-        if(strcmp(source_name, argv[1]) == 0)
-        {
-            bool b = false;
-            for (vector<neighboure>::iterator it = neighborhood.begin(); it != neighborhood.end(); it++) {
-                if (it -> name == destination_name) {
-                    it->port_from_me_to_neighboure = source_port;
-                    b = true;
-                }
-            }
-            if (!b) {
-                neighboure n;
-                n.name = destination_name;
-                n.cost = cost;
-                n.port_from_me_to_neighboure = source_port;
-                n.port_from_neighboure_to_me = -1;
-                neighborhood.push_back(n);
-            }
-        }
-        if(strcmp(destination_name, argv[1]) == 0)
-        {
-            if (neighborhood.empty()) {
-                neighboure n;
-                n.name = source_name;
-                n.cost = cost;
-                n.port_from_me_to_neighboure = -1;
-                n.port_from_neighboure_to_me = source_port;
-                neighborhood.push_back(n);
-            }
-            bool inTheNeighbor = false;
-            for (vector<neighboure>::iterator it = neighborhood.begin(); it != neighborhood.end(); it++) {
-                if (it -> name == source_name) {
-                    inTheNeighbor = true;
-                }
-            }
-            if (inTheNeighbor == false) {
-                neighboure n;
-                n.name = source_name;
-                n.cost = cost;
-                n.port_from_me_to_neighboure = -1;
-                n.port_from_neighboure_to_me = source_port;
-                neighborhood.push_back(n);
-            }
-            for(vector<neighboure>::iterator it = neighborhood.begin();
-                it != neighborhood.end(); it++)
-            {
-                if(it->name == source_name)
-                {
-                    it->port_from_neighboure_to_me = source_port;
-                }
-            }
-        }
-        line = "";
-    }
-    cout << "Current node: " << argv[1] << endl << "Neighboures: " << endl << endl;
-    for (vector<neighboure>::iterator it = neighborhood.begin(); it != neighborhood.end(); it++) {
-        cout << "Neighboure's name: " << it->name << endl << "Cost:" << it->cost << endl;
-        cout << "Neighboure's port number: " <<it->port_from_neighboure_to_me << endl;
-        cout << "Current Node's port number: " <<it -> port_from_me_to_neighboure << endl << endl;
-    }
-    
-    
-    // while (fgets(sendline, 10000,stdin) != NULL)
-    // {
-    //    sendto(sockfd,sendline,strlen(sendline),0,
-    //           (struct sockaddr *)&servaddr,sizeof(servaddr));
-    //    n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
-    //    recvline[n]=0;
-    //    fputs(recvline,stdout);
-    // }
-    
+	if(argc < 2)
+	{
+		cout << "Missing router name!" << endl;
+		return 1;
+	}
+    manager m = manager("./initialize.txt", argv[1]);
+    m.print_out();
+    cout << "The manager will now try to make the routers communicate..." << endl << endl;
+    m.communicate();
     return 0;
 }
-
